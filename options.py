e41696ce0bbb3507088094b9c6c2736d42027a37
your_username <taleh.muzaffarov@dirs21.team>
2021-11-26 10:00:40 +0100

    Home Options

--- options.py
+++ options.py
@@ -1,4 +1,5 @@
+from telebot import types
 
 HOME_OPTIONS = []
 for option in ['Order product','Check order status', 'Review Product', 'Cancel Order', 'Abort Order','Change Language','Rules', 'Help']:
-     HOME_OPTIONS.append(InlineKeyboardButton(option, callback_data = option))
+     HOME_OPTIONS.append(types.InlineKeyboardButton(option, callback_data = option))
