c529731357b242548eee19e4eabbec75f5b604fb
your_username <taleh.muzaffarov@dirs21.team>
2021-11-26 09:39:09 +0100

    initial

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://warm-sands-89104.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://infinite-crag-95236.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
